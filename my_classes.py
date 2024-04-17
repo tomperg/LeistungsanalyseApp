3fd7dcef7f0ae36b268482979527f65ebb0fa4d8
Eric345343434 <je3301@mci4me.at>
2024-04-17 10:58:54 +0200

    my_classes

---
+++ my_classes.py
@@ -0,0 +1,17 @@
+class Person():
+    def __innit__(self, name,age)
+        self.age= age
+        self.name= name
+        
+
+
+class Experiment(): 
+    def __init__(self, name, date, supervisor, subject):
+        self.name = name
+        self.date = date
+        self.supervisor = supervisor
+        self.subject = subject
+
+
+ 
+
